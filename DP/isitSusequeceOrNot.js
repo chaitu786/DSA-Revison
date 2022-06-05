1a0bd538bf5e99da536a436e717b8b07792cffe5
chaitu786 <chaithanyareddy786@gmail.com>
2022-06-05 20:22:42 +0530

    hi

--- DP/isitSusequeceOrNot.js
+++ DP/isitSusequeceOrNot.js
@@ -1,26 +1,36 @@
+
 function runProgram(input){
     input=input.trim().split("\n")
-    let n=+input[0]
-    let dp=[]
-    for(i=0;i<=n;i++){
-        dp.push(-1)
+    let tc=+input[0]
+    let line=1
+    for(i=0;i<tc;i++){
+        let s1=input[line++].trim()
+        let s2=input[line++].trim()
+        if(sub(s1,s2)){
+            console.log("YES");
+        }
+        else{
+            console.log("NO");
+        }
+
     }
-    console.log(nWays(n,dp));
 }
-const nWays=(n,dp)=>{
-    if(n==0){
-        return dp[n]=1
+function sub(s1,s2){
+    let j=0
+    for(i=0;i<s1.length&&j<s2.length;i++){
+        if(s1[j]==s2[i]){
+            j++
+        }
     }
-    if(n<0){
-        return dp[n]=0
-    }
-    if(dp[n]!=-1){
-        return dp[n]
-    }
-    return dp[n]=nWays(n-1,dp)+nWays(n-3,dp)+nWays(n-4,dp)
+    return j==s2.length
 }
 if (process.env.USERNAME === "Chaithanya") {
-  runProgram(`5`);
+  runProgram(`
+2
+abc
+adbce
+ax
+xaa`);
 } else {
   process.stdin.resume();
   process.stdin.setEncoding("ascii");
